7f9031e82c641ae3a141e51f8706d552be09d48c
Khaled Attia <khaled.attia@microdoers.com>
2017-11-05 20:27:18 +0200

    A method that takes a string and prints the alphabetical order of each character

---
+++ Lab03/charOrder/charOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace charOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+            printOrder(word);
+        }
+
+        static void printOrder(string s)
+        {
+            string lowerWord = s.ToLower();
+            foreach (char ch in lowerWord)
+            {
+                Console.Write((int)(ch - 'a'));
+            }
+        }
+    }
+}
