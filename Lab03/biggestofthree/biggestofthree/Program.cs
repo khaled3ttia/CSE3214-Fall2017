06671357b07aba5db6f9d3b6c2a055a84ee1cb67
Khaled Attia <khaled.attia@microdoers.com>
2017-11-05 20:40:37 +0200

    A method that calculates the biggest of two numbers, then use it to calculate the biggest of three numbers

---
+++ Lab03/biggestofthree/biggestofthree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace biggestofthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The largest of three number is : {0}", myMax(myMax(a, b), c));
+        }
+
+        static int myMax(int x, int y)
+        {
+            return x > y ? x : y;
+        }
+    }
+}
