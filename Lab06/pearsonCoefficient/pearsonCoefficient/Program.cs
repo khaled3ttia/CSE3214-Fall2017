11ebb13a9e513c56d8179f1a0cce72a2b6aadd3a
Khaled Attia <khaled.3ttia@gmail.com>
2017-12-06 13:12:55 +0200

    Lab 06 source code and pdf

---
+++ Lab06/pearsonCoefficient/pearsonCoefficient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace pearsonCoefficient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines("data.txt");
+            int n = lines.Length;
+            double[] x = new double[n];
+            double[] y = new double[n];
+            double[] xy = new double[n];
+            double[] xSquare = new double[n];
+            double[] ySquare = new double[n];
+            double sumX=0, sumY=0, sumxSquare=0, sumySquare=0, sumxy=0;
+            for (int i=0; i< n; i++)
+            {
+                string[] line = lines[i].Split(':');
+                x[i] = double.Parse(line[0]);
+                sumX += x[i];
+                y[i] = double.Parse(line[1]);
+                sumY += y[i];
+
+                xy[i] = x[i] * y[i];
+                sumxy += xy[i];
+
+                xSquare[i] = Math.Pow(x[i], 2);
+                sumxSquare += xSquare[i];
+
+                ySquare[i] = Math.Pow(y[i], 2);
+                sumySquare += ySquare[i];
+            }
+            double r = ((n * sumxy) - (sumX * sumY)) / Math.Sqrt(((n * sumxSquare) - Math.Pow(sumX, 2)) * ((n * sumySquare) - Math.Pow(sumY, 2)));
+            Console.WriteLine("r is {0}", r);
+        }
+    }
+}
---
+++ Lab06/studentCSV/studentCSV/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace studentCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("student.csv", true);
+                Console.WriteLine("File opened/created successfully, ready to input data");
+                Console.WriteLine("Enter student name, type exit if you want to stop");
+                string name = Console.ReadLine();
+
+                while (name != "exit")
+                {
+                    Console.WriteLine("Enter student email:");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("Enter student phone number");
+                    string phone = Console.ReadLine();
+                    sw.WriteLine(name + "," + email + "," + phone);
+                    Console.WriteLine("===========");
+                    Console.WriteLine("Enter student name, type exit if you want to stop");
+                    name = Console.ReadLine();
+                }
+                sw.Close();
+            }
+            catch (System.IO.IOException)
+            {
+                Console.WriteLine("File already in use, please close if open in another program");
+                return;
+            }
+            
+        }
+    }
+}
